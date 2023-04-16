7798e14dfece986e826c249c1c99eb9dd43c4c28
viswa <127508366+viswa-fusion@users.noreply.github.com>
2023-04-16 10:53:50 +0530

    Add files via upload

---
+++ indiamap/mapdata.js
@@ -0,0 +1,269 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#404241",
+    state_hover_color: "#3B729F",
+    state_url: "#",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "20px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman And Nicobar",
+      description: "The Andaman and Nicobar Islands are a union territory in India and have a small but significant agricultural sector. The territory is known for producing coconut, betel nut, and other crops.",
+      hover_color: "#00CED1"
+    },
+    "2": {
+      name: "Andhra Pradesh",
+      description: "Andhra Pradesh is a coastal state in southern India and is known for its agricultural production. The state is a major producer of rice, tobacco, and other crops.",
+      hover_color: "#A52A2A"
+    },
+    "3": {
+      name: "Arunachal Pradesh",
+      description: "Arunachal Pradesh is a northeastern state in India and has a diverse agricultural sector. The state is known for producing oranges, tea, and other crops.",
+      hover_color: "#E6E6FA"
+    },
+    "4": {
+      name: "Assam",
+      description: "Assam is a northeastern state in India and has a significant agricultural sector. The state is known for producing tea, rice, and other crops.",
+      hover_color: "#556B2F"
+    },
+    "5": {
+      name: "Bihar",
+      description: "Bihar is a state in eastern India and has a significant agricultural sector. The state is known for producing rice, sugarcane, and other crops.",
+      hover_color: "#ADD8E6"
+    },
+    "6": {
+      name: "Chandigarh",
+      description: "Chandigarh is a union territory and serves as the capital of both Punjab and Haryana. It has a relatively small agricultural sector, and most of its food supply comes from nearby states.",
+      hover_color: "#00BFFF"
+    },
+    "7": {
+      name: "Chhattisgarh",
+      description: "Chhattisgarh is a central state in India and has a significant agricultural sector. The state is known for producing rice, pulses, and other crops.",
+      hover_color: "#00FF00"
+    },
+    "8": {
+      name: "Dadra And Nagar Haveli",
+      description: "Dadra and Nagar Haveli is a union territory located in western India. The region has a relatively small agricultural sector, and its economy is primarily based on industry and manufacturing.",
+      hover_color: "#F0E68C"
+    },
+    "9": {
+      name: "Daman And Diu",
+      description: "Daman and Diu is a union territory located on the west coast of India. The region has a relatively small agricultural sector, and its economy is primarily based on tourism and manufacturing.",
+      hover_color: "#BDB76B"
+    },
+    "10": {
+      name: "Delhi",
+      description: "Delhi is a union territory and the capital city of India. It has a relatively small agricultural sector, and most of its food supply comes from nearby states.",
+      hover_color: "#DDA0DD"
+    },
+    "11": {
+      name: "Goa",
+      description: "Goa is a small coastal state in western India and has a relatively small agricultural sector. The state is known for producing coconuts, cashews, and other crops.",
+      hover_color: "#FFFF00"
+    },
+    "12": {
+      name: "Gujarat",
+      description: "Gujarat is a western state in India and has a significant agricultural sector. The state is known for producing cotton, groundnut, and other crops.",
+      hover_color: "#90EE90"
+    },
+    "13": {
+      name: "Haryana",
+      description: "Haryana is a northern state in India and has a significant agricultural sector. The state is known for producing wheat, rice, and other crops.",
+      hover_color: "#FF4500"
+    },
+    "14": {
+      name: "Himachal Pradesh",
+      description: "Himachal Pradesh is a northern state in India and has a significant agricultural sector. The state is known for producing apples, potatoes, and other crops.",
+      hover_color: "#A9A9A9"
+    },
+    "16": {
+      name: "Jharkhand",
+      description: "Jharkhand is an eastern state in India and has a significant agricultural sector. The state is known for producing rice, wheat, and other crops.",
+      hover_color: "#A0522D"
+    },
+    "17": {
+      name: "Karnataka",
+      description: "Karnataka is a state in southern India and has a diverse agriculture sector. The state is known for producing coffee, sugarcane, and other crops.",
+      hover_color: "#FF0000"
+    },
+    "18": {
+      name: "Kerala",
+      description: "Kerala is a small state in southern India and has a significant agricultural sector. The state is known for producing coconut, rubber, and other crops.",
+      hover_color: "#006400"
+    },
+    "19": {
+      name: "Lakshadweep",
+      description: "Lakshadweep is a union territory in India and has a small but significant agricultural sector. The territory is known for producing coconut and other crops.",
+      hover_color: "#BA55D3"
+    },
+    "20": {
+      name: "Madhya Pradesh",
+      description: "Madhya Pradesh is a central state in India and has a significant agricultural sector. The state is known for producing soybean, wheat, and other crops.",
+      hover_color: "#FFA500"
+    },
+    "21": {
+      name: "Maharashtra",
+      hover_color: "#FFFF00",
+      description: "Maharashtra is one of the most industrialized states in India, but it also has a significant agricultural sector. The state is known for producing sugarcane, cotton, and other crops."
+    },
+    "22": {
+      name: "Manipur",
+      description: "Manipur is a northeastern state in India and has a diverse agricultural sector. The state is known for producing rice, potatoes, and other crops.",
+      hover_color: "#FFD700"
+    },
+    "23": {
+      name: "Meghalaya",
+      hover_color: "#3CB371",
+      description: "Meghalaya is a northeastern state in India and has a significant agricultural sector. The state is known for producing rice, maize, and other crops."
+    },
+    "24": {
+      name: "Mizoram",
+      description: "Mizoram is a northeastern state in India and has a significant agricultural sector. The state is known for producing rice, maize, and other crops.",
+      hover_color: "#20B2AA"
+    },
+    "25": {
+      name: "Nagaland",
+      description: "Nagaland is a northeastern state in India and has a significant agricultural sector. The state is known for producing rice, maize, and other crops.",
+      hover_color: "#9400D3"
+    },
+    "26": {
+      name: "Odisha",
+      description: "Odisha is a coastal state in eastern India and has a significant agricultural sector. The state is known for producing rice, pulses, and other crops.",
+      hover_color: "#800000"
+    },
+    "27": {
+      name: "Puducherry",
+      description: "Puducherry is a union territory located in southern India. The region has a relatively small agricultural sector, and its economy is primarily based on tourism and manufacturing.",
+      hover_color: "#9370DB"
+    },
+    "28": {
+      name: "Punjab",
+      hover_color: "#008000",
+      description: "Known as the \"Granary of India\", Punjab is one of the leading agricultural states in the country. It is a major producer of wheat, rice, and other crops."
+    },
+    "29": {
+      name: "Rajasthan",
+      description: "Rajasthan is a northern state in India and has a diverse agricultural sector. The state is known for producing wheat, mustard, and other crops.",
+      hover_color: "#FFC0CB"
+    },
+    "30": {
+      name: "Sikkim",
+      description: "Sikkim is a small state in northeastern India and has a significant agricultural sector. The state is known for producing cardamom, ginger, and other crops.",
+      hover_color: "#87CEEB"
+    },
+    "31": {
+      name: "Tamil Nadu",
+      description: "Tamil Nadu is a southern state in India and has a significant agricultural sector. The state is known for producing rice, sugarcane, and other crops.",
+      hover_color: "#FFFF00"
+    },
+    "32": {
+      name: "Tripura",
+      description: "Tripura is a northeastern state in India and has a diverse agricultural sector. The state is known for producing rice, jute, and other crops.",
+      hover_color: "#9932CC"
+    },
+    "33": {
+      name: "Uttar Pradesh",
+      hover_color: "#008000",
+      description: "Uttar Pradesh is the most populous state in India and has a diverse agricultural sector. It is a major producer of sugarcane, wheat, rice, and other crops."
+    },
+    "34": {
+      name: "Uttarakhand",
+      description: "Uttarakhand is a northern state in India and has a diverse agricultural sector. The state is known for producing rice, wheat, and other crops.",
+      hover_color: "#808000"
+    },
+    "35": {
+      name: "West Bengal",
+      description: "West Bengal is a state in eastern India and has a diverse agricultural sector. The state is known for producing rice, tea, and other crops.",
+      hover_color: "#00008B"
+    },
+    "36": {
+      name: "Jammu And Kashmir",
+      description: "Jammu and Kashmir is a northern state in India and has a significant agricultural sector. The state is known for producing apples, saffron, and other crops.",
+      hover_color: "#D3D3D3"
+    },
+    "37": {
+      name: "Telangana",
+      description: "Telangana is a southern state in India and has a diverse agricultural sector. The state is known for producing cotton, paddy, and other crops.",
+      hover_color: "#800080"
+    },
+    "38": {
+      name: "Ladakh",
+      description: "Ladakh is a union territory located in northern India. The region has a cold desert climate and a relatively small agricultural sector.",
+      hover_color: "#B0C4DE"
+    }
+  },
+  locations: {},
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
+function backHome(){
+  window.history.go(-1);
+}
